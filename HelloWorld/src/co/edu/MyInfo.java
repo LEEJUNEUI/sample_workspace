dba9508c8ba444f5fe0958c97773350e61f33e24
admin <admin@YD-16>
2022-07-01 12:52:29 +0900

    push

---
+++ HelloWorld/src/co/edu/MyInfo.java
@@ -0,0 +1,18 @@
+package co.edu;
+
+public class MyInfo {
+	public static void main(String[] args) {
+		// 이름, 나이, 키
+		String myName = " 홍길동";	
+		int myAge = 16;
+		double myHeight = 178.8;
+		
+		myName = " 김민수";
+		myAge = 22;
+		myHeight = 173.1;
+	
+		
+		System.out.println("이름은" + myName + "이고" + " 나이는 " + myAge + "살이고" + " 키는 " + myHeight + "cm입니다.");
+				
+	}
+}
