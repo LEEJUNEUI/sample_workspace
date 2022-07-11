5043343f3265658b70f5ae336eafa5e7cf9aefef
admin <admin@YD-16>
2022-07-11 17:50:54 +0900

    8

---
+++ 20220711/src/co/edu/MorningEx2.java
@@ -0,0 +1,27 @@
+package co.edu;
+
+import java.util.Scanner;
+
+public class MorningEx2 {
+	public static void main(String[] args) {
+		// 정수(int) 배열 6개
+		// 1~ 100까지 임의값;
+		Scanner scn = new Scanner(System.in);
+		int[] intAry = new int[6];
+		int max = intAry[0];
+		int sum = 0;
+		double avg = 0;
+
+		for (int i = 0; i < intAry.length; i++) {
+			intAry[i] = (int) (Math.random() * 100) + 1;
+			sum += intAry[i];
+		}
+		avg = (double) sum / intAry.length;
+		
+		// 확장 for
+		for (int num : intAry) {
+			System.out.println(num);
+		}
+
+	}
+}
